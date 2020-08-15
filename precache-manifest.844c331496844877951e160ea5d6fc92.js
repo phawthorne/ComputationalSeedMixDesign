7fe07bd194856d401a95e92fe069cd4c0e004d29
Peter Hawthorne <hawt0010@umn.edu>
2020-08-14 20:42:26 -0500

    Updates

---
+++ precache-manifest.844c331496844877951e160ea5d6fc92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90a216de6a095e7bad1698a29580998",
+    "url": "/computational-seed-mix-design/index.html"
+  },
+  {
+    "revision": "b75c045754181d7ea47a",
+    "url": "/computational-seed-mix-design/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "049f6dd86e0ccd8854a3",
+    "url": "/computational-seed-mix-design/static/js/2.1f473c1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/computational-seed-mix-design/static/js/2.1f473c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75c045754181d7ea47a",
+    "url": "/computational-seed-mix-design/static/js/main.702a6654.chunk.js"
+  },
+  {
+    "revision": "0f5aa9eb6a4b77992243",
+    "url": "/computational-seed-mix-design/static/js/runtime-main.6f18d0c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/computational-seed-mix-design/static/media/logo.5d5d9eef.svg"
+  }
+]);
